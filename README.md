9e2cde234ddac82f362ab74500b7db7849751008
CLHF <charleslhf@gmail.com>
2019-04-26 17:05:48 -0400

    Migrated code from old repo

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Route-Pothole
+# Topics
+
+## Published topics for external use
+
+  - `geometry_msgs::Twist` on **/rb_obstcale/rb_obstcale/twist_cmd**
+
+# Nodes
+
+
+## actor_obstacle
+
+Take input from all obstacle nodes and decide a velocity command to send based
+on obstacle priority. Send a large velocity if no obstacles are detected.
+
+  - Publish:
+    - **Twist on "~/twist_cmd"**: `linear.z` will be used as a urgency rate 0.0 = no urgency, 1.0 = high urgency
+    - **Uint32 on "/turn_flag/flag"**: Sends flags to the turn state machine
+  - Subscribe:
+    - Point on "/actor_obstacle/front_obstacle/obstacle_loc"
+    - Point on "/actor_obstacle/road_obstacle/obstacle_loc"
+    - UInt8 on "actor_obstacle/stop_sign_detection/stop_sign"
+
+
+## road_obstacle
+
+Look for obstructions in the car's path using the long-range top lidar
+
+  - Publish:
+    - **Point on "~/obstcale_loc"**: Represents approximate distance from nearest obstacle
+    - **LaserScan on "~/debug_scan"**: Scan message displaying only objects that have been detected
+  - Subscribe:
+    - LaserScan on "/actor_input/lidar_top/scan"
+
+
+## front_obstacle
+
+Look for obstructions in the car's path using the front lidar.
+
+  - Publish:
+    - **Point on "~/obstacle_loc"**: Represents approximate distance from nearest obstacle
+    - **LaserScan on "~/debug_scan"**: Scan showing only points detected as an obstacle (use in rviz)
+  - Subscribe:
+    - LaserScan on "/actor_input/lidar_front/scan"
+
+## stop_sign_detection
+
+Detects if there is a stop sign in front of the car.
+
+  - Publish:
+    - **UInt8 on "~/stop_sign"**: Boolean value, true if sign detected, false otherwise
+    - **UInt32 on "~/sign_size"**: Size of sign in pixels
+  - Subscribe:
+    - <params/front_cam_in> *(loaded from config/params.yaml)*
---
+++ src/pothole_detection.cpp
@@ -0,0 +1,197 @@
+#include <ros/ros.h>
+#include <std_msgs/UInt32.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <ltu_actor_route_pothole/PotholeConfig.h>
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+/* Pothole Detection
+ * A Class that detects if there is a pothole in front of the car.
+ *
+ * Publish:     UInt8 on "~/stop_sign"
+ *              UInt32 on "~/sign_size"
+ * Subscribe: <params/front_cam_in>
+ */
+class PotholeDetection
+{
+
+public:
+    PotholeDetection();
+
+private:
+    // Callbacks
+    void imageCb(const sensor_msgs::ImageConstPtr& msg);
+    void configCB(actor_obstacle::PotholeConfig &config, uint32_t level);
+
+    // Run sign detection
+    void detectAndDisplay(cv::Mat& frame);
+
+    // ROS Objects
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_; // debug pub
+    ros::Publisher trigger_pub_;
+
+    // Dynamic reconfigure server
+    dynamic_reconfigure::Server<actor_obstacle::PotholeConfig> server_;
+    actor_obstacle::PotholeConfig config_;
+
+    bool pothole_detected_;
+};
+
+PotholeDetection::PotholeDetection() : nh_("~"), it_(nh_)
+{
+    // Subscribe to camera
+    std::string dashcam_topic;
+    //if (nh_.getParam("/actor_input/dashcam", dashcam_topic))
+    //{
+        //ROS_INFO_STREAM("blob: using video source " << dashcam_topic << "...");
+    //}
+    //else
+    //{
+        //dashcam_topic = "/actor_input/camera/image_raw/";
+        //ROS_ERROR_STREAM("param '/actor_input/dashcam' not defined, using default topic: " << dashcam_topic);
+    //}
+
+    dashcam_topic = "/actor_input/cam_repub/image_raw";
+
+    image_sub_ = it_.subscribe(dashcam_topic, 1, &PotholeDetection::imageCb, this);
+
+    // Dynamic reconfigure
+    server_.setCallback(boost::bind(&PotholeDetection::configCB, this, _1, _2));
+    server_.getConfigDefault(config_);
+
+    trigger_pub_ = nh_.advertise<std_msgs::String>("/actor_nav/trigger", 10);
+    image_pub_ = it_.advertise("debug", 1);
+
+    pothole_detected_ = false;
+}
+
+void PotholeDetection::configCB(actor_obstacle::PotholeConfig &config, uint32_t level)
+{
+    config_ = config;
+}
+
+static int clamp(int v, int lo, int hi)
+{
+    return std::max(lo+1, std::min(hi-1, v));
+}
+
+/** @function detectAndDisplay */
+void PotholeDetection::detectAndDisplay( cv::Mat& frame )
+{
+    cv::Mat frame_gray;
+
+
+    int width = frame.cols;
+    int height = frame.rows;
+
+
+    // Get roi values
+    int xlo = clamp(config_.xlo, 0,         width/2);
+    int xhi = clamp(config_.xhi, 0,   width);
+    int ylo = clamp(config_.ylo, 0,         height/2);
+    int yhi = clamp(config_.yhi, 0,  height);
+
+    // Generate b&w roi
+    auto roi_rect = cv::Rect(xlo, ylo, xhi, yhi);
+    cv::cvtColor( frame, frame_gray, CV_BGR2GRAY );
+
+    ROS_INFO_STREAM(xlo << ", " << ylo << "; " << xhi << ", " << yhi);
+    auto roi = cv::Mat(frame_gray, roi_rect);
+
+    // Count white pixels in roi
+    cv::threshold(roi, roi, clamp(config_.bin_thresh, 0, 255), 255, cv::THRESH_TOZERO);
+    int count = cv::countNonZero(roi);
+    bool found = count > config_.pixel_count_thresh;
+
+
+    // Display rect
+    const int thickness = 6;
+    if (found)
+    {
+        cv::rectangle(frame, roi_rect, cv::Scalar(0,255,0), thickness);
+    }
+    else
+    {
+        cv::rectangle(frame, roi_rect, cv::Scalar(255,0,0), thickness/2);
+    }
+    
+    ROS_INFO_STREAM("count: " << count << ", found: " << found);
+
+
+    // Count the number of valid consecutive sign frames
+    //    Only publish a detection if the number of frames is
+    //    above a certain threshold (frame_count_trigger_)
+    static int frame_count = 0;
+
+    //ROS_INFO_STREAM("sign frames: " << frame_count);
+    if (found) {
+        frame_count++;
+        if (frame_count > config_.frame_count_trigger) {
+            pothole_detected_ = true;
+        }
+    } else {
+        pothole_detected_ = false;
+        frame_count = 0;
+    }
+
+    // Publish sign
+    if (pothole_detected_)
+    {
+        std_msgs::String trigger_msg;
+        trigger_msg.data = "POTHOLE";
+        trigger_pub_.publish(trigger_msg);
+    }
+
+
+}
+
+void PotholeDetection::imageCb(const sensor_msgs::ImageConstPtr& msg)
+{
+    //Convert to cv image
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    detectAndDisplay(cv_ptr->image);
+
+    if (image_pub_.getNumSubscribers() > 0) {
+            image_pub_.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_ptr->image).toImageMsg());
+    }
+
+    cv::waitKey(3);
+
+}
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "pothole_detection");
+    PotholeDetection pd;
+
+    ros::spin();
+    return 0;
+}
